0ed18ce2b8fd07170b3d609659b6258fe54ef61d
Ivan Shumilov <ivan.shumilov91@gmail.com>
2020-07-02 22:42:37 +0300

    more tests added

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -14,4 +14,20 @@ class RadioTest {
         assertEquals(6, radio.getCurrentStation());
     }
 
+    @Test
+    void shouldSetNextStationLoop() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.setNextStation();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldSetPreviousStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(6);
+        radio.setPreviousStation();
+        assertEquals(5, radio.getCurrentStation());
+    }
+
 }
